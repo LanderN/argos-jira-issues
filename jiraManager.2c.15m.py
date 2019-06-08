5f3c974ab8891d4a0fa71a2429ecbaafd410b936
Lander Noterman <lander.noterman@gmail.com>
2019-06-08 16:43:02 +0200

    clean up code (somewhat)

--- jiraManager.2c.15m.py
+++ jiraManager.2c.15m.py
@@ -8,9 +8,9 @@ from datetime import datetime
 import requests
 from jira import JIRA
 
-jiraLogo = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAMZJREFUOI2Vkj0OAUEAhb9IJOKn3PM5gEaCWqOROIIrKPWicAm9hEoidHyaXdaameUl00zevL8MpDEFZjWcKDL1ql6ALEZqJAT6QBvoAuO/7dWNbyRTxATOJQGB+T/vW+rDTwRTxDZ4BO66wKjOOQNWQE89+Y3aLeZ53yWwDggkt8jUS867A4uQQDVFeYNx3rO4bwKnyBZf/6LsXmAHTH5J8epewQHoqMe6LULuhQC/pAi5q25zgXQKda/eqgcYlCoOQxx1/wRTboLP64okfwAAAABJRU5ErkJggg=='
+# LOAD CONFIGURATION
 
-os. chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # TODO: error handling, file creation
 with open('.jira_credentials.json', 'r') as f:
@@ -18,6 +18,45 @@ with open('.jira_credentials.json', 'r') as f:
 with open('.jira_reviewers.json', 'r') as f:
     reviewers = json.load(f)
 
+jira = JIRA(credentials['host'],
+            basic_auth=(credentials['username'],
+                        credentials['password']))
+
+# COMMAND LINE INVOCATION
+
+if (len(sys.argv) > 1):
+  if (len(sys.argv) == 2 and str(sys.argv[1]) == "users"):
+    print("Username\t(Display Name)")
+    print("--------\t--------------")
+    for user in jira.search_users(''):
+      print(user.key + '\t(%s)' % user.displayName)
+
+  elif (len(sys.argv) == 4 and str(sys.argv[1]) == "transition"):
+    issue = sys.argv[2]
+    transition = sys.argv[3]
+
+    if (transition == "Start progress"):
+      timelog = open(".jira_progressstarted.txt", "w+")
+      timelog.write(str(datetime.now().timestamp()))
+      timelog.close()
+
+    if (transition == "Resolved" or transition == "Stop progress"):
+      timelog = open(".jira_progressstarted.txt", "r")
+      timeStarted = datetime.fromtimestamp(float(timelog.readline()))
+      timeNow = datetime.now()
+      diffMinutes = (timeNow - timeStarted).seconds / 60
+      jira.add_worklog(issue, timeSpent=str(diffMinutes) + 'm', comment="Auto-logged by JIRA Issue Manager")
+      os.remove(".jira_progressstarted.txt")
+
+    if (transition == "Resolved"):
+      jira.transition_issue(issue, transition, assignee=sys.argv[4])
+    else:
+      jira.transition_issue(issue, transition)
+
+  exit(0)
+
+
+# HELPER FUNCTIONS
 
 def getAsBase64(url):
     return base64.b64encode(requests.get(url).content).decode('utf-8')
@@ -57,45 +96,14 @@ def addLinkToIssue(issue, subMenu=False):
   addMenuItem("%sShow issue in browser..." % ('', '--')[subMenu], {'href': link, 'iconName': 'application-exit'})
 
 
-jira = JIRA(credentials['host'],
-            basic_auth=(credentials['username'],
-                        credentials['password']))
-
-if (len(sys.argv) == 2 and str(sys.argv[1]) == "users"):
-  print("Username\t(Display Name)")
-  print("--------\t--------------")
-  for user in jira.search_users(''):
-    print(user.key + '\t(%s)' % user.displayName)
-  exit(0)
-
-if (len(sys.argv) == 4 and str(sys.argv[1]) == "transition"):
-  issue = sys.argv[2]
-  transition = sys.argv[3]
-
-  if (transition == "Start progress"):
-    timelog = open(".jira_progressstarted.txt", "w+")
-    timelog.write(str(datetime.now().timestamp()))
-    timelog.close()
-    pass
-
-  if (transition == "Resolved" or transition == "Stop progress"):
-    timelog = open(".jira_progressstarted.txt", "r")
-    timeStarted = datetime.fromtimestamp(float(timelog.readline()))
-    timeNow = datetime.now()
-    diffMinutes = (timeNow - timeStarted).seconds / 60
-    jira.add_worklog(issue, timeSpent=str(diffMinutes) + 'm', comment="Auto-logged by JIRA Issue Manager")
-    pass
-
-  if (transition == "Resolved"):
-    jira.transition_issue(issue, transition, assignee=sys.argv[4])
-  else:
-    jira.transition_issue(issue, transition)
+# MAIN PROGRAM START
 
 nextIssues = getIssueWithStatus('Next')
 inProgressIssues = getIssueWithStatus('In Progress')
 
 if (len(inProgressIssues) == 0):
-  addMenuItem('Not working... :coffee:\n---')
+  addMenuItem('Not working... :coffee:')
+  addSeparator()
   if (len(nextIssues) == 0):
     addMenuItem('Put issues in "Next" to work on them')
   for issue in nextIssues:
@@ -110,7 +118,9 @@ if (len(inProgressIssues) == 0):
     addLinkToIssue(issue, subMenu=True)
 
 elif (len(inProgressIssues) > 1):
-  addMenuItem('Can only have 1 issue in progress!')
+  addMenuItem('You may only have 1 issue with status "In progress"!')
+  addMenuItem('Use JIRA to fix this')
+  addSeparator()
 
 else:
   issue = inProgressIssues[0]
@@ -129,5 +139,6 @@ else:
   addLinkToIssue(issue)
 
 addSeparator()
-addMenuItem("Open JIRA", {'href': credentials["host"], 'image': jiraLogo})
+
+addMenuItem("Open JIRA", {'href': credentials["host"], 'image': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAMZJREFUOI2Vkj0OAUEAhb9IJOKn3PM5gEaCWqOROIIrKPWicAm9hEoidHyaXdaameUl00zevL8MpDEFZjWcKDL1ql6ALEZqJAT6QBvoAuO/7dWNbyRTxATOJQGB+T/vW+rDTwRTxDZ4BO66wKjOOQNWQE89+Y3aLeZ53yWwDggkt8jUS867A4uQQDVFeYNx3rO4bwKnyBZf/6LsXmAHTH5J8epewQHoqMe6LULuhQC/pAi5q25zgXQKda/eqgcYlCoOQxx1/wRTboLP64okfwAAAABJRU5ErkJggg=='})
 addMenuItem("Refresh", {'bash': '', 'iconName': 'object-rotate-right'})
